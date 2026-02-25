7e0c8f6fd7c6bfb2069a8add2377e604422377a7
Chris Chatelain <chris.chatelain@digiex.ca>
2026-02-25 09:08:44 -0600

    Fix gradle build for unsigned prebuild

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = null
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
